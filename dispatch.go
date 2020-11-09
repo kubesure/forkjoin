f7151da55a3a904872bd60c494be993bfaa70a13
kubesure <edakghar@gmail.com>
2020-11-09 09:51:21 +0400

    moved worker algo here

---
+++ dispatch.go
@@ -0,0 +1,55 @@
+package forkjoin
+
+import "time"
+
+//implements the template worker algo for all workers and dispatches work to worker
+func dispatch(done <-chan interface{}, i input, w Worker, pulseInterval time.Duration) (<-chan Result, <-chan heartbeat) {
+
+	pulseStream := make(chan heartbeat)
+	resultStream := make(chan Result)
+	c := make(chan Result)
+	quit := make(chan interface{})
+
+	go func() {
+		defer close(quit)
+		defer close(c)
+		defer close(resultStream)
+		//dispatches work to worker
+		go w.work(done, i.x, c)
+		for {
+			select {
+			case <-done:
+				return
+			case r := <-c:
+				resultStream <- r
+				quit <- struct{}{}
+				return
+			default:
+			}
+		}
+	}()
+
+	go func() {
+		defer close(pulseStream)
+		pulse := time.Tick(pulseInterval)
+		sendPulse := func() {
+			select {
+			case pulseStream <- heartbeat{}:
+			default:
+			}
+		}
+		//send pulse at a interval or 1 second
+		for {
+			select {
+			case <-done:
+				return
+			case <-quit:
+				return
+			case <-pulse:
+				sendPulse()
+			default:
+			}
+		}
+	}()
+	return resultStream, pulseStream
+}
--- manager.go
+++ manager.go
@@ -1,4 +1,4 @@
-package main
+package forkjoin
 
 import (
 	"context"
@@ -12,7 +12,7 @@ func init() {
 	log.SetFlags(log.Ltime)
 }
 
-//goroutine waits for response from checker on work channel.
+//goroutine waits for response from worker on work channel.
 func manage(ctx context.Context, i input, multiplexdResultStream chan<- Result) {
 	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	workStream := make(chan Result)
@@ -72,55 +72,4 @@ func manage(ctx context.Context, i input, multiplexdResultStream chan<- Result) 
 			}
 		}
 	}()
-}
-
-//implements the worker algo for all workers and dispatches work to worker
-func dispatch(done <-chan interface{}, i input, w Worker, pulseInterval time.Duration) (<-chan Result, <-chan heartbeat) {
-
-	pulseStream := make(chan heartbeat)
-	resultStream := make(chan Result)
-	c := make(chan Result)
-	quit := make(chan interface{})
-
-	go func() {
-		defer close(c)
-		defer close(resultStream)
-		//dispatches work to worker
-		go w.work(done, i.x, c)
-		for {
-			select {
-			case <-done:
-				return
-			case r := <-c:
-				resultStream <- r
-				quit <- struct{}{}
-				return
-			default:
-			}
-		}
-	}()
-
-	go func() {
-		defer close(pulseStream)
-		pulse := time.Tick(pulseInterval)
-		sendPulse := func() {
-			select {
-			case pulseStream <- heartbeat{}:
-			default:
-			}
-		}
-		//send pulse at a interval or 1 second
-		for {
-			select {
-			case <-done:
-				return
-			case <-quit:
-				return
-			case <-pulse:
-				sendPulse()
-			default:
-			}
-		}
-	}()
-	return resultStream, pulseStream
 }
