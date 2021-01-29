2a72c148d3dd1de3603939f0db8bf7116acc2e35
kubesure <edakghar@gmail.com>
2021-01-29 21:43:54 +0400

    added test cases for full coverage

--- httpworker_test.go
+++ httpworker_test.go
@@ -12,6 +12,36 @@ func init() {
 	log.SetFlags(log.Ltime)
 }
 
+func TestInvalidHttpMethod(t *testing.T) {
+	msg := HTTPMessage{URL: "https://httpbin.org/anything"}
+	reqMsg := HTTPRequest{Message: msg}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	m := NewMultiplexer()
+	m.AddWorker(&HTTPDispatchWorker{})
+	resultStream := m.Multiplex(ctx, reqMsg)
+	for r := range resultStream {
+		if r.err == nil {
+			t.Errorf("should have given dispatch config error")
+		}
+	}
+}
+
+func TestEmptyHttpURL(t *testing.T) {
+	msg := HTTPMessage{Method: GET}
+	reqMsg := HTTPRequest{Message: msg}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	m := NewMultiplexer()
+	m.AddWorker(&HTTPDispatchWorker{})
+	resultStream := m.Multiplex(ctx, reqMsg)
+	for r := range resultStream {
+		if r.err == nil {
+			t.Errorf("should have given dispatch config error")
+		}
+	}
+}
+
 func TestHttpGETDispatch(t *testing.T) {
 	msg := HTTPMessage{Method: GET, URL: "https://httpbin.org/anything"}
 	msg.Add("header1", "value1")
@@ -76,8 +106,40 @@ func TestHttpPOSTDispatch(t *testing.T) {
 				if len(res.Message.Headers) == 0 {
 					t.Errorf("response headers is empty")
 				}
-				log.Println(res)
+				//log.Println(res)
 			}
+		}
+	}
+}
+
+func TestHttpGETDeplyedResponse(t *testing.T) {
+	msg := HTTPMessage{Method: GET, URL: "http://localhost:8000/healthz"}
+	reqMsg := HTTPRequest{Message: msg}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	m := NewMultiplexer()
+	m.AddWorker(&HTTPDispatchWorker{})
+	resultStream := m.Multiplex(ctx, reqMsg)
+	for r := range resultStream {
+		if r.err != nil {
+			log.Printf("Error for id: %v %v\n", r.id, r.err.Message)
+		} else {
+			t.Errorf("response not expected check test")
+		}
+	}
+}
+
+func TestHttpURLError(t *testing.T) {
+	msg := HTTPMessage{Method: GET, URL: "http://unknown:8000/healthz"}
+	reqMsg := HTTPRequest{Message: msg}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	m := NewMultiplexer()
+	m.AddWorker(&HTTPDispatchWorker{})
+	resultStream := m.Multiplex(ctx, reqMsg)
+	for r := range resultStream {
+		if r.err == nil {
+			t.Errorf("Should have failed to connect to unknown host")
 		}
 	}
 }
