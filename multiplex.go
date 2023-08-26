b46bbe458520c5810d38567265567908f775efec
kubesure <edakghar@gmail.com>
2023-08-26 16:24:40 +0400

    moved code into one file multiplex.go

--- multiplex.go
+++ multiplex.go
@@ -2,15 +2,17 @@ package forkjoin
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"time"
 )
 
-//NewMultiplexer creates new basic multiplexer
+// NewMultiplexer creates new basic multiplexer
 func NewMultiplexer() Multiplexer {
 	return Multiplexer{}
 }
 
-//AddWorker adds workers to multiplex on N worker
+// AddWorker adds workers to multiplex on N worker
 func (m *Multiplexer) AddWorker(w Worker) {
 	if m.workers == nil {
 		m.workers = make([]Worker, 0)
@@ -18,7 +20,7 @@ func (m *Multiplexer) AddWorker(w Worker) {
 	m.workers = append(m.workers, w)
 }
 
-//Multiplex starts N goroutines configured in []config
+// Multiplex starts N goroutines configured in []config
 func (m *Multiplexer) Multiplex(ctx context.Context, x interface{}) <-chan Result {
 
 	if len(m.workers) == 0 {
@@ -39,4 +41,80 @@ func (m *Multiplexer) Multiplex(ctx context.Context, x interface{}) <-chan Resul
 		wg.Wait()
 	}()
 	return multiplexdResultStream
+}
+
+// goroutine waits for response from worker on work channel.
+func manage(ctx context.Context, i input, multiplexdResultStream chan<- Result) {
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(i.worker.ActiveDeadLineSeconds())*time.Second)
+	defer cancel()
+	defer i.wg.Done()
+
+	log := NewLogger()
+
+	sendResult := func(r Result) {
+		multiplexdResultStream <- r
+	}
+
+	const pulseInterval = 1 * time.Second
+	resultStream, pulseStream := dispatch(ctx, i, i.worker, pulseInterval)
+	lastPulseT := time.Now()
+
+	//worker moniter loop
+	for {
+		select {
+		case hb := <-pulseStream:
+			currPulseT := time.Now()
+			diff := int32(currPulseT.Sub(lastPulseT).Seconds())
+			lastPulseT = currPulseT
+			if diff > 2 {
+				log.LogInfo(fmt.Sprint(hb.id), "Heartbeat inconsistent spawning new worker goroutine")
+				resultStream, pulseStream = dispatch(ctx, i, i.worker, pulseInterval)
+			}
+		case r := <-resultStream:
+			sendResult(r)
+			return
+		}
+	}
+}
+
+// implements the template worker algo for all workers and dispatches work to worker
+func dispatch(ctx context.Context, i input, w Worker, pulseInterval time.Duration) (<-chan Result, <-chan Heartbeat) {
+
+	pulseStream := make(<-chan Heartbeat)
+	result := make(<-chan Result)
+	resultStream := make(chan Result)
+	//quitPulseStream := make(chan interface{})
+
+	go func() {
+		//defer close(quitPulseStream)
+		defer close(resultStream)
+		result, pulseStream = w.Work(ctx, i.x)
+		resultStream <- <-result
+		//quitPulseStream <- struct{}{}
+	}()
+
+	return resultStream, pulseStream
+}
+
+// TODO how to make worker whom this lib has no control implement send pulse?
+// SendPulse to be called by worker to send heart beat back to manager
+func SendPulse(ctx context.Context, pulseStream chan Heartbeat, pulseInterval time.Duration) {
+	defer close(pulseStream)
+	pulse := time.NewTicker(pulseInterval)
+
+	sendPulse := func() {
+		select {
+		case pulseStream <- Heartbeat{}:
+		default:
+		}
+	}
+	//send pulse at a interval or 1 second
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-pulse.C:
+			sendPulse()
+		}
+	}
 }
